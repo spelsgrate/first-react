ac77749ba569007ea6409a9f8ee1e3b2e458aded
spelsgrate <antemratin@gmail.com>
2020-03-04 15:40:02 -0500

    init commit

---
+++ src/components/SomeClassComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class SomeClassComponent extends Component {
+    render() {
+        return <div>
+            <h1>hello, World!</h1>
+        </div>;
+    }
+}
+export default SomeClassComponent;
